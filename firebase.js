a9b8692a941a33277278a1e7f681b3f56bcc0289
mehlmanu5-ui <mehlmanu5@gmail.com>
2026-07-08 19:55:23 +0200

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -13,25 +13,63 @@ from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 import { getStorage } 
 from "https://www.gstatic.com/firebasejs/12.15.0/firebase-storage.js";
 
+
+
+/* =========================
+   FIREBASE CONFIG
+========================= */
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBWzfkGLulCyTfWnr0elYXgDzn2SSYzHcc",
-  authDomain: "tech-board-8bdd5.firebaseapp.com",
-  projectId: "tech-board-8bdd5",
-  storageBucket: "tech-board-8bdd5.appspot.com",
-  messagingSenderId: "668103046596",
-  appId: "1:668103046596:web:51c6e0348d7f1fba1ba091"
+
+    apiKey: "AIzaSyBWzfkGLulCyTfWnr0elYXgDzn2SSYzHcc",
+
+    authDomain:
+    "tech-board-8bdd5.firebaseapp.com",
+
+    projectId:
+    "tech-board-8bdd5",
+
+    storageBucket:
+    "tech-board-8bdd5.firebasestorage.app",
+
+    messagingSenderId:
+    "668103046596",
+
+    appId:
+    "1:668103046596:web:51c6e0348d7f1fba1ba091"
+
 };
 
-const app = initializeApp(firebaseConfig);
 
+
+
+
+/* =========================
+   INITIALIZE
+========================= */
+
+
+const app =
+initializeApp(firebaseConfig);
+
+
+
+
+
+/* =========================
+   EXPORTS
+========================= */
 
 
 export const db =
 getFirestore(app);
 
 
+
 export const auth =
 getAuth(app);
+
 
 
 export const storage =
